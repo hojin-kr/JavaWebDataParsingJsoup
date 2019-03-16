1a71899546479f7ba01872424aeeadb588278cc5
hojin <jhj377@gmail.com>
2019-03-16 16:40:07 +0900

    epl 작동확인, Jsoup을 사용한 DOM select 접근을 하고자 하였으나, javascript 이슈로 인해서 일단 Json 형식으로 분리하여 작업

--- src/main/java/com/hojin/java_web_data_parsing_jsoup/FormLeagueInfo.java
+++ src/main/java/com/hojin/java_web_data_parsing_jsoup/FormLeagueInfo.java
@@ -1,4 +1,9 @@
 package com.hojin.java_web_data_parsing_jsoup;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.oracle.tools.packager.Log;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -6,6 +11,9 @@ import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class FormLeagueInfo {
     private final String league_name;
@@ -14,7 +22,6 @@ public class FormLeagueInfo {
     //생서자, 클래스 호출시 수행
     public FormLeagueInfo(String league_name){
         this.league_name = league_name;
-
 
     }
 
@@ -30,31 +37,55 @@ public class FormLeagueInfo {
 
     //Jsoup으로 웹 파싱하여 데이터 구성
     public void usingJsoupParsing(){
-        switch (league_name){
-            //프리미어리그
-            case "epl":try{
+        try{
+            Document doc = Jsoup.connect("https://sports.news.naver.com/wfootball/record/index.nhn?category=epl").get();
+            String TeamRecord = doc.toString();
+            //split 메소드로 json 부분만 남김
+            String[] TeamRecords = TeamRecord.split("jsonTeamRecord:");
+            TeamRecords = TeamRecords[1].split("teamRecordBodyName");
+            //TeamRecords[0] = TeamRecords[0].replaceAll("\\\"","'");
+            TeamRecords = TeamRecords[0].split(",\\\n");
 
-                for(int i=0;i<10;i++){
-                    //팀별로 스코어 점수 매칭 시키는 코드 작성
+            //json 파싱
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(TeamRecords[0]);
 
-                    FormLeague_Rank league_rank = new FormLeague_Rank();
+            //regularTeamRecordList 추출
+            JSONArray regularTeamRecordListArray = (JSONArray) jsonObject.get("regularTeamRecordList");
 
-                    league_rank.setRank(1);
-                    league_rank.setTeam("리버플 FC");
-                    league_rank.setGame_count(27);
 
-                    league_ranks.add(league_rank);
+            switch (league_name){
+                //프리미어리그
+                case "epl":
+                    for(int i=0;i<regularTeamRecordListArray.size();i++){
+                        //팀별로 스코어 점수 매칭 시키는 코드 작성
 
-                }
-            }catch(Exception e){} //예외
-            break;
-            //라리가
-            case "primera":try{
-                Document doc = Jsoup.connect("https://sports.news.naver.com/wfootball/record/index.nhn?category=epl").get();
+                        //순차 접근
+                        JSONObject eplRecordObject = (JSONObject) regularTeamRecordListArray.get(i);
 
-            }catch(Exception e){} //예외
-                break;
+                        FormLeague_Rank league_rank = new FormLeague_Rank();
 
-        }
+                        league_rank.setRank(Integer.parseInt(eplRecordObject.get("rank").toString()));
+                        league_rank.setTeam(eplRecordObject.get("teamName").toString());
+                        league_rank.setGame_count(Integer.parseInt((eplRecordObject.get("gameCount").toString())));
+                        league_rank.setWin_score(Integer.parseInt(eplRecordObject.get("gainPoint").toString()));
+                        league_rank.setWin(Integer.parseInt(eplRecordObject.get("won").toString()));
+                        league_rank.setDraw(Integer.parseInt(eplRecordObject.get("drawn").toString()));
+                        league_rank.setLose(Integer.parseInt(eplRecordObject.get("lost").toString()));
+                        league_rank.setGet_point(Integer.parseInt(eplRecordObject.get("gainGoal").toString()));
+                        league_rank.setLose_point(Integer.parseInt(eplRecordObject.get("loseGoal").toString()));
+                        league_rank.setDiff_point(Integer.parseInt(eplRecordObject.get("goalGap").toString()));
+
+                        league_ranks.add(league_rank);
+                    }
+                    break;
+                //라리가
+                case "primera":
+                    break;
+
+            }
+
+        }catch(Exception e){}
+
     }
 }
